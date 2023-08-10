ef7405582ac07f58a8cf2142fec6aece16ce5eba
רפאל אדרי <you@example.com>
2023-08-10 17:06:30 +0300

    delet

--- projuct/homePage.js
+++ projuct/homePage.js
@@ -1,7 +1,7 @@
 const data = [
   {
     
-    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+   
     price: 109.95,
     description:
       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
@@ -9,7 +9,7 @@ const data = [
     image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
     rating: { rate: 3.9, count: 120 },
   },
-  {
+  { 
     id: 2,
     title: "Mens Casual Premium Slim Fit T-Shirts ",
     price: 22.3,
